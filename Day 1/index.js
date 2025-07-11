a1494e2eaacf73fca244046131260c9c1c236942
Priyadharshan.B <priyadharshan0409@gmail.com>
2025-07-11 10:50:01 +0530

    Add files via upload

---
+++ Day 1/index.js
@@ -0,0 +1,16 @@
+var x = 3;
+let name = "js";
+const iscloudy = true;
+
+console.log(name + " " + iscloudy + " " + x);
+
+document.write(name);
+document.write(x);
+document.write(iscloudy);
+function add(a, b) {
+  console.log("this is a function" + (a + b));
+}
+add(4, 5);
+let a = parseInt(prompt("Enter the first value"));
+let b = parseInt(prompt("Enter the first value"));
+alert(a + b);
